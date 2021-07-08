8439c4d10a9f37eaa3644b7695ffea0334eef947
Jakub Lasek <jakublasek47@gmail.com>
2021-07-08 14:53:08 +0200

    Fix background with list title header

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,13 +3,13 @@ import styles from './Hero.scss';
 
 
 const Hero = () => (
-  <section className={styles.component}>
-        <h2 className={styles.title}>Things to do!</h2>
+  <header className={styles.component}>
+        <h2 className={styles.title}>Things to do</h2>
         <img
         className={styles.image}
         src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"
         />
-  </section>
+  </header>
 );
 
 export default Hero;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Hero from '../Hero/Hero.js';
 import styles from './List.scss';
 
 class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <h2>List</h2>
+        <Hero />
       </section>
     )
   }
