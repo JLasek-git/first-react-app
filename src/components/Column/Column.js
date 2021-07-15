f2ad5c4505864fb2d6ee1156737479c55e84e6b2
Jakub Lasek <jakublasek47@gmail.com>
2021-07-15 11:30:04 +0200

    Add columnsRedux.js and columns generator

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 // import Creator from '../Creator/Creator.js';
 import Card from '../Card/Card.js';
 import Icon from './Icon.js';
+import { settings } from '../../data/dataStore';
 // import { settings} from '../../data/dataStore';
 
 
@@ -12,6 +13,10 @@ class Column extends React.Component {
       title: PropTypes.node.isRequired,
       cards: PropTypes.array,
       icon: PropTypes.string,
+    }
+
+    static defaultProps = {
+      icon: settings.defaultColumnIcon,
     }
 
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,7 +5,7 @@ import Column from '../Column/ColumnContainer';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 
 
 class List extends React.Component {
@@ -15,6 +15,7 @@ class List extends React.Component {
     description: PropTypes.node,
     columns: PropTypes.array,
     image: PropTypes.string,
+    addColumn: PropTypes.func,
   }
 
   static defaultProps = {
@@ -24,7 +25,7 @@ class List extends React.Component {
 
 
   render() {
-    const {title, image, description, columns} = this.props;
+    const {title, image, description, columns, addColumn} = this.props;
     return (
       <section className={styles.component}>
         <Hero titleText={title} photo={image} />
@@ -36,10 +37,9 @@ class List extends React.Component {
             <Column key={columnData.id} {...columnData} />
           ))}
         </div>
-        {/*
         <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
-        </div> */}
+          <Creator text={settings.columnCreatorText} action={addColumn} />
+        </div>
       </section>
     );
   }
