6a6a7864a65e8a3eb12182d405c6ea75cba974a4
Jakub Lasek <jakublasek47@gmail.com>
2021-07-17 22:05:54 +0200

    Add function to add cards using redux

--- redux/store-dataStore/store.js
+++ redux/store-dataStore/store.js
@@ -1,6 +1,7 @@
 import {combineReducers, createStore} from 'redux';
 import initialStoreData from '../../src/data/dataStore';
 import ColumnsReducer from '../columnsRedux';
+import CardsReducer from '../cardsRedux';
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -13,6 +14,7 @@ const initialState = {
 // define reducers
 const reducers = {
   columns: ColumnsReducer,
+  cards: CardsReducer,
 };
 
 // add blank reducers for initial state properties without reducers
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Card from '../Card/Card.js';
 import Icon from './Icon.js';
 import { settings } from '../../data/dataStore';
@@ -13,6 +13,7 @@ class Column extends React.Component {
       title: PropTypes.node.isRequired,
       cards: PropTypes.array,
       icon: PropTypes.string,
+      addCard: PropTypes.func,
     }
 
     static defaultProps = {
@@ -21,7 +22,7 @@ class Column extends React.Component {
 
 
     render(){
-      const {title, icon, cards} = this.props;
+      const {title, icon, cards, addCard} = this.props;
       return(
         <section className={styles.component}>
           <h3 className={styles.title}>
@@ -35,10 +36,9 @@ class Column extends React.Component {
               <Card key={cardData.id} {...cardData} />
             ))}
           </div>
-          {/*
           <div>
-            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-          </div> */}
+            <Creator text={settings.cardCreatorText} action={addCard} />
+          </div>
         </section>
       );
     }
